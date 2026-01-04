7fd2742579c425dbd81e301170bc38a93768c385
ChuksTechy <155270881+ChuksTechy@users.noreply.github.com>
2026-01-04 12:58:13 +0100

    Comment out S3 backend configuration in backend.tf
    Comment out the Terraform backend configuration.

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 # --- root/backend.tf ---
 
-terraform {
-  backend "s3" {
-    bucket = "joseph-tfdev-bucket"
-    key    = "remote.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#  backend "s3" {
+#    bucket = "joseph-tfdev-bucket"
+#    key    = "remote.tfstate"
+#    region = "us-east-1"
+#  }
+#}
